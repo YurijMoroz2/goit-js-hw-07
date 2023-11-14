c9002065da33cee671d8a6adb05ff2206387f39b
YurijMoroz2 <139611781+YurijMoroz2@users.noreply.github.com>
2023-11-14 22:00:51 +0200

    homeWork-07-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const container = document.querySelector(".gallery");
 console.log(galleryItems);
+console.log(container);
+
+function createMarkup(arr){
+  const res= arr.map(({preview, original, description}) =>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  ).join("");
+  
+  return res;
+};
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+// add click
+
+container.addEventListener("click", handelClick);
+function handelClick(event) {
+  event.preventDefault()
+
+  if (event.target === event.currentTarget) {
+      return;
+  };
+  const modalImg = event.target.closest(".gallery__link").getAttribute("href");
+  console.log(modalImg)
+ 
+//  -----------------------------------------------------------
+let gallery = new SimpleLightbox('.gallery__item a',{ captionsData: "alt", captionDelay: 250});
+gallery.on('show.simplelightbox', function () {
+	// Do something…
+    `<img class="gallery__image" src="${modalImg}" alt="${modalImg.description}">
+      `;
+});
+// -----------------------------------------------------------
+  document.addEventListener("keydown", (event) => {
+      if (event.code === "Escape") {
+          return gallery.close();
+      }
+      return;
+  })
+};
